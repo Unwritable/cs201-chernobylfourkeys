9cad336f895e92960608db5f6b2756179e3e27d8
Hydralyph <acidicmc@gmail.com>
2022-05-08 01:47:02 +1200

    Updated the RadiationManager.cs script file. Created a new CheckDistance function alongside the general system setup.

--- Assets/Scripts/RadiationManager.cs
+++ Assets/Scripts/RadiationManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class RadiationManager : MonoBehaviour
 {
     private static RadiationManager _instance;
+    private GameObject _player;
+    private GameObject _currentClosestRadSource;
     private float _currentRads { get; set; } = 0.0f;
     [field : SerializeField, Range(1000, 100000)] private float _maxRads { get; set; }
     [field: SerializeField, Range(2, 25)] private float _baseRadsIncrease { get; set; }
@@ -32,14 +34,41 @@ public class RadiationManager : MonoBehaviour
         _instance = this;
     }
 
+
     private void Start()
     {
+        // Get a reference to the player ready for distance checks
+        _player = GameObject.FindGameObjectWithTag("Player");
+
+        // Find and add the radiation sources we will be checking the distance from the player on
         _radSources.AddRange(GameObject.FindGameObjectsWithTag("RadSource"));
         foreach(GameObject rs in _radSources)
         {
-            Debug.Log($"RadiationManager :: AddRadSourcesCheck: {rs.name}");
+            Debug.Log($"RadiationManager :: AddRadSources Check: {rs.name}");
         }
+
+
     }
 
-    // ADD RADIATION CODE HERE, INCLUDING THE COROUTINE AND POSSIBLY UPDATE METHOD
+    private GameObject CheckDistance(GameObject _player, List<GameObject> _radSources)
+    {
+        GameObject ret = null;
+
+        // Set this to a arbitrarily high value, as we want to get the radSource if it is closer
+        float distMax = 1000;
+
+        foreach(GameObject gm in _radSources)
+        {
+            float dist = Vector3.Distance(gm.transform.position, _player.transform.position);
+            if (dist < distMax)
+            {
+                distMax = dist;
+                ret = gm;
+            }
+        }
+
+        return ret;
+    }
+    
+
 }
